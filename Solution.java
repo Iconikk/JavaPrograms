4d648d43184d87352ab7c76fe6a1911d931ac85e
Tejas Nikas <tejasnikas122@gmail.com>
2026-01-19 00:11:04 +0530

    Update add some new stocks and other problems

---
+++ Solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+ public class Solution {
+    
+
+public int maxProfit(int[] prices) {
+     int buy = prices[0]; 
+     int profit = 0; 
+
+     for (int i=1; i<prices.length; i++) { 
+        if (buy < prices[i]) { 
+            profit = Math.max(prices[i] - buy, profit); 
+        } 
+        else { 
+            buy = prices[i];
+
+         } 
+        }
+         return profit;
+    }
+public static void main(String[] args) {
+    Solution sol = new Solution();
+    int[] prices = {7,1,5,3,6,4};
+    System.out.println(sol.maxProfit(prices)); // Output: 5
+  }
+} 
